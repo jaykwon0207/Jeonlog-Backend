ce9ec669978af0bd00fe267a0a8abfdd9d440e30
jaykwon0207 <zoidshj0119@gmail.com>
2025-07-26 22:48:26 +0900

    init: spring boot project

---
+++ src/main/java/com/jeonlog/exhibition_recommender/ExhibitionRecommenderApplication.java
@@ -0,0 +1,13 @@
+package com.jeonlog.exhibition_recommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExhibitionRecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExhibitionRecommenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeonlog/exhibition_recommender/ExhibitionRecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeonlog.exhibition_recommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExhibitionRecommenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
