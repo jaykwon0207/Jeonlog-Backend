ad67b6222e3a87cebb842293299dc69bc196af19
Jeongyeun <ruthie0219@naver.com>
2025-08-28 22:01:12 +0900

    장르타입 수정

--- src/main/java/com/jeonlog/exhibition_recommender/exhibition/domain/GenreType.java
+++ src/main/java/com/jeonlog/exhibition_recommender/exhibition/domain/GenreType.java
@@ -1,6 +1,15 @@
 package com.jeonlog.exhibition_recommender.exhibition.domain;
 
 public enum GenreType {
-    MAIN,
-    THEME
+    MODERN_ART,        // 현대미술
+    PAINTING,          // 회화
+    PHOTO,             // 사진
+    SCULPTURE,         // 조각/설치
+    DIGITAL,           // 디지털아트
+    ILLUSTRATION,      // 일러스트/그래픽
+    CRAFT,             // 공예/도예
+    CLASSIC_ART,       // 해외명화/고전미술
+    TRADITIONAL,       // 전통미술/민화
+    NATURE_SCIENCE,    // 자연/과학 전시
+    EXPERIENCE         // 체험형 전시
 }
