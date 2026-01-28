f834eca5a74a4dcf7a4bf9737d799aa8e33b48b5
dlwldn30 <dlwldn30@naver.com>
2026-01-28 10:23:41 +0900

    dkdk

--- src/main/java/com/jeonlog/exhibition_recommender/user/controller/ProfileController.java
+++ src/main/java/com/jeonlog/exhibition_recommender/user/controller/ProfileController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/profile")
-public class ProfileController {
+public class ProfileController { // ss
 
     private final ProfileService profileService;
 
